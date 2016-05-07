5a647a9804ec30d8057a857f67390484cdf64a5d
AdelGilm <adel.gilm@gmail.com>
2016-05-07 12:45:45 +0300

    sql queries were added

---
+++ SchoolProject/core/src/main/java/ru/kpfu/itis/Gilmanova/sql/create.sql
@@ -0,0 +1,90 @@
+﻿CREATE TABLE "students"(
+	"id" INT NOT NULL,
+	"last_name" VARCHAR(50), 
+    "first_name"  VARCHAR(50),
+    "class_id" integer,
+	"user_id" integer,
+	PRIMARY KEY("id")
+	FOREIGN KEY ("class_id") REFERENCES "classes"("id")
+	FOREIGN KEY ("user_id") REFERENCES "users"("id")
+	);
+	
+CREATE TABLE "teachers"(
+	"id" INT NOT NULL,
+	"last_name"  VARCHAR(50) NOT NULL,
+    "first_name"  VARCHAR(50) NOT NULL,
+	"second_name" VARCHAR(50) NOT NULL,
+	"gender" VARCHAR(10) NOT NULL,
+	"birthday" DATE NOT NULL,
+	"user_id" integer,
+	PRIMARY KEY("id")
+	FOREIGN KEY ("user_id") REFERENCES "users"("id")
+	);
+
+CREATE TABLE "objects"(
+	"id" INT NOT NULL,
+	"object" VARCHAR(50) NOT NULL,
+	PRIMARY KEY("id")
+	);
+
+CREATE TABLE "classes"(
+	"id" INT NOT NULL,
+	"teacher_id" INT NOT NULL,
+    "class" VARCHAR(50) NOT NULL,
+	PRIMARY KEY("id"),
+	FOREIGN KEY ("teacher_id") REFERENCES "teachers"("id")
+	);
+	
+CREATE TABLE "questions"(
+	"id" INT NOT NULL,
+    "theme" VARCHAR(500) NOT NULL,
+    "question_content" VARCHAR(1000) NOT NULL,
+	"email" VARCHAR(100) NOT NULL,
+	"question_name" VARCHAR(100) NOT NULL,
+	"question_time" TIME NOT NULL,
+	"question_date" DATE NOT NULL,
+	PRIMARY KEY("id")
+	);
+	
+CREATE TABLE "users"(
+  id integer NOT NULL,
+  username character varying(50) NOT NULL,
+  password character varying(50) NOT NULL,
+  role character varying(100),
+  enable boolean,
+  PRIMARY KEY("id")
+);
+	
+CREATE TABLE "comments"(
+    "id" integer primary key,
+    "user_name" VARCHAR(50),
+    "content" VARCHAR(1000),
+    "time" TIME NOT NULL,
+    "date" DATE NOT NULL
+);	
+
+create table teacher_object(
+	id integer not null primary key,
+	teacher_id integer,
+	object_id integer,
+	FOREIGN KEY ("teacher_id") REFERENCES "teachers"("id"),
+	FOREIGN KEY ("object_id") REFERENCES "objects"("id")
+);
+
+CREATE TABLE home_work(
+	id integer NOT NULL PRIMARY KEY,
+	class_teacher_object_id integer,
+	home_work varchar,
+	"time" time,
+	"date" date, 
+	FOREIGN KEY ("class_teacher_object_id") REFERENCES "class_teacher_object"("id")
+);
+
+create table class_teacher_object(
+	id integer not null primary key,
+	class_id integer,
+	teacher_object_id integer,
+	foreign key (class_id) references classes(id),
+	foreign key (teacher_object_id) references teacher_object(id)
+);
+
---
+++ SchoolProject/core/src/main/java/ru/kpfu/itis/Gilmanova/sql/data.sql
@@ -0,0 +1,54 @@
+﻿insert into students values(20,'Галеев','Василий', 20, 1);
+insert into students values(21,'Говорков','Игорь', 21, 1);
+insert into students values(22,'Комаров','Мурат', 22, 2);
+insert into students values(23,'Загитова','Аделина', 23, 2);
+insert into students values(24,'Гафаров','Руслан', 24, 3);
+insert into students values(25,'Охотников','Стас', 25, 3);
+insert into students values(26,'Перескокова','Ольга', 26, 4);
+insert into students values(27,'Шикиров','Марат', 27, 4);
+
+insert into teachers values(01,'Петрова','Луиза','Николаевна','женский','05.10.1960', '/resources/images/photo.png', 1);
+insert into teachers values(02,'Альбина','Галина','Григорьевна','женский','06.10.1961', '/resources/images/photo.png', 2);
+insert into teachers values(03,'Крайнова','Ольга','Николаевна','женский','07.10.1962', '/resources/images/photo.png', 3);
+insert into teachers values(04,'Григорьева','Татьяна','Борисовна','женский','08.10.1963', '/resources/images/photo.png', 4);
+insert into teachers values(05,'Хисамова','Альфира','Радиковна','женский','09.10.1964', '/resources/images/photo.png', 5);
+insert into teachers values(06,'Зарезнова','Елена ','Георгиевна','женский','10.10.1965', '/resources/images/photo.png', 6);
+insert into teachers values(07,'Садыков','Жаудат','Назипович','мужской','11.10.1960', '/resources/images/photo.png', 7);
+insert into teachers values(08,'Киселева','Оксана','Вячеславовна','женский','12.10.1970', '/resources/images/photo.png', 8);
+insert into teachers values(09,'Галихина','Ляля','Раисовна','женский','13.10.1975', '/resources/images/photo.png', 9);
+insert into teachers values(10,'Тигина','Ольга','Александровна','женский','14.10.1966', '/resources/images/photo.png', 10);
+insert into teachers values(11,'Ахметзанова','Гулия','Талгатовна','женский','15.10.1988', '/resources/images/photo.png', 11);
+insert into teachers values(12,'Ибатуллина','Рузиля','Рауфовна','женский','16.10.1970', '/resources/images/photo.png', 12);
+insert into teachers values(13,'Мансурова','Рита','Хабибовна','женский','17.10.1960', '/resources/images/photo.png', 13);
+insert into teachers values(14,'Вальтонов','Виктор','Николаевич','мужской','18.10.1970', '/resources/images/photo.png', 14);
+insert into teachers values(15,'Гайфуллина','Айгуль','Закиевна','женский','19.10.1979', '/resources/images/photo.png', 15);  
+
+insert into objects values(1,'Алгебра');
+insert into objects values(2,'Английский язык');
+insert into objects values(3,'Биология');
+insert into objects values(4,'География');
+insert into objects values(5,'Геометрия');
+insert into objects values(6,'Информатика и ИКТ');
+insert into objects values(7,'История');
+insert into objects values(8,'Литература');
+insert into objects values(9,'Обществознание');
+insert into objects values(10,'Русский язык');
+insert into objects values(11,'Татарская литература');
+insert into objects values(12,'Татарский язык');
+insert into objects values(13,'Физика');
+insert into objects values(14,'Физическая культура'); 
+insert into objects values(15,'Химия');
+
+insert into classes values(1,01,'11А');
+insert into classes values(2,09,'11Б');
+insert into classes values(3,07,'11В');
+insert into classes values(4,03,'11Г');
+
+insert into questions values(1,'ремонт школы','кому сдавать деньги на ремонт школы?','ksjdfs@mail.ru','Petrova Alina','08:30','10.10.2015');
+insert into questions values(2,'новые учебники','когда в библиотеке появяться новые учебники?','gurhggk@mail.ru','Zalipov Aleksey','10:30','11.10.2015');
+insert into questions values(3,'питание','сколько в этом году стоит обед в школьной столовой?','hgyrkd@gmail.com','Gerapov Ildar','09:30','12.10.2015');
+
+
+
+
+
